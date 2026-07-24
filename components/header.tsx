b88db71346eaa7a1b6b1795091b8109cd5edb57f
claudio gomez <clauivangomez48@gmail.com>
2026-07-23 23:49:49 -0300

    refactor(routing): consolidate pages into root

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import Link from "next/link"
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 flex justify-between items-center px-4 py-3 border-b-[0.5px] border-zinc-800/80 backdrop-blur-md" >
           <div>LOGO</div>
-          <nav className="flex gap-4">
-            <Link href="/">Home</Link>
-            <Link href="/tecnology">Tecnologies</Link>
-            <Link href="/project">Projects</Link>
-          </nav>
         </header>
   )
 }
