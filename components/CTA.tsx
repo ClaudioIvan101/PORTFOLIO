5db9bc6f2703b79eb6f491cbe4d60caec419bccb
claudio gomez <clauivangomez48@gmail.com>
2026-07-28 00:05:28 -0300

    changes in technologies

---
+++ components/CTA.tsx
@@ -0,0 +1,14 @@
+export default function CTA() {
+    return (
+      <section className="py-24 text-center">
+        <h2 className="text-4xl font-bold">Let's talk about your project.</h2>
+        <p className="mt-4 text-lg text-gray-600 max-w-xl mx-auto">
+          Got a project in mind? Let's create something great. 
+          We can transform that idea into real product.
+        </p>
+        <a href="#contacto" className="mt-8 inline-block px-6 py-3 rounded-full bg-black text-white">
+        Let's talk
+        </a>
+      </section>
+    )
+  }
---
+++ components/hero.tsx
@@ -0,0 +1,11 @@
+export default function hero()  {
+  return (
+    <div>
+      <section className="">
+        <h1><span className="text-orange-500">WELCOME</span> TO MY PORTFOLIO</h1>
+        <p>I&apos;m a software engineer with a passion for building web applications and mobile applications.</p>
+        <button className="bg-orange-500 text-white px-4 py-2 rounded-md">CONTACT ME</button>
+      </section>
+    </div>
+  )
+}
